36095ad624e8bc134a821bdd136aad680e63ac53
Andrej Kolčin <self@kaathewise.net>
2025-05-03 16:24:41 +0300

    [b3] priors/coalescent: add stub

---
+++ crates/b3/b3/priors/coalescent.py
@@ -0,0 +1,12 @@
+from b3 import State, Tree
+
+
+class ConstantPopulation:
+    def __init__(self, tree: Tree):
+        self.tree = tree
+
+    # TODO
+    def probability(self, state: State) -> float: ...
+
+
+# TODO: Skyline
