19850974e8574870d7693b95990d1882b2da5fa4
Andrej Kolčin <self@kaathewise.net>
2025-05-22 15:58:14 +0300

    [aspartik,b3] Add README

--- python/aspartik/__init__.py
+++ python/aspartik/__init__.py
@@ -1,10 +1,5 @@
 """Computational biology toolkit for Python powered by Rust.
 
-Aspartik contains a suit of programs developed for the sake of the `b3`
-Bayesian phylogenetic inference engine.  Most of them are currently very raw
-and only expose the functionality required by `b3`, but I hope that as
-development continues they'll become broadly useful.
-
 - `b3`: Bayesian phylogenetic analysis engine, analogous to BEAST2.
 - `data`: biological data classes, currently only include DNA.
 - `io`: bioinformatics file formats parsers.
