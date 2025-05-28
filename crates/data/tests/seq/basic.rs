9ab41b2219738f086b71a3417f97ed9b23137804
Andrej Kolčin <self@kaathewise.net>
2025-05-28 22:33:39 +0300

    [data] tests: create
    I don't like having tests in the file, so I think I'll have all of them
    in `tests/`.  Now, if only I could get a reliable AI to refactor `stats`
    this way and be sure that it didn't mess up anything in the process.
    Perhaps coming up with a script would be easier and faster.

---
+++ crates/data/tests/seq/basic.rs
@@ -0,0 +1,63 @@
+use data::seq::*;
+use data::DnaNucleotide::*;
+
+#[test]
+fn decode() {
+	parse_str::<DnaSeq>("ACTGxACTG").unwrap_err();
+}
+
+#[test]
+fn count() {
+	let s = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
+	let s: DnaSeq = parse_str(s).unwrap();
+
+	assert_eq!(s.count(Adenine), 20);
+	assert_eq!(s.count(Cytosine), 12);
+	assert_eq!(s.count(Guanine), 17);
+	assert_eq!(s.count(Thymine), 21);
+}
+
+#[test]
+fn dna_complement() {
+	let s: DnaSeq = parse_str("AAAACCCGGT").unwrap();
+
+	assert_eq!(s.reverse_complement().to_string(), "ACCGGGTTTT");
+}
+
+#[test]
+fn hamming() {
+	let s1: DnaSeq = parse_str("GAGCCTACTAACGGGAT").unwrap();
+	let s2: DnaSeq = parse_str("CATCGTAATGACGGCCT").unwrap();
+
+	assert_eq!(s1.hamming_distance(s2), 7);
+}
+
+#[test]
+fn index() {
+	let mut s: DnaSeq = parse_str("ACGT").unwrap();
+	assert_eq!(s[0], Adenine);
+	assert_eq!(s[1], Cytosine);
+	assert_eq!(s[2], Guanine);
+	assert_eq!(s[3], Thymine);
+
+	s[0] = Thymine;
+	s[1] = Cytosine;
+	s[2] = Guanine;
+	s[3] = Adenine;
+	assert_eq!(s[0], Thymine);
+	assert_eq!(s[1], Cytosine);
+	assert_eq!(s[2], Guanine);
+	assert_eq!(s[3], Adenine);
+}
+
+#[test]
+fn iter() {
+	let s: DnaSeq = parse_str("GAGCCT").unwrap();
+	let mut iter = s.iter().copied();
+	assert_eq!(iter.next(), Some(Guanine));
+	assert_eq!(iter.next(), Some(Adenine));
+	assert_eq!(iter.next(), Some(Guanine));
+	assert_eq!(iter.next(), Some(Cytosine));
+	assert_eq!(iter.next(), Some(Cytosine));
+	assert_eq!(iter.next(), Some(Thymine));
+}
