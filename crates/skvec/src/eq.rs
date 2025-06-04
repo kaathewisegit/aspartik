bca3e19cf1f7cbcd6caaddaa98b38ed1a375f452
Andrej Kolčin <self@kaathewise.net>
2025-06-04 14:39:11 +0300

    [skvec] eq: refactor `PartialEq` implementation and add `Eq`
    Since all of the implementations used `impl_inside` I moved the whole
    `impl` block inside the macro.  Also added `Eq`, whose lack I noticed
    when I wasn't able to derive it on `Parameter`.

--- crates/skvec/src/eq.rs
+++ crates/skvec/src/eq.rs
@@ -2,60 +2,36 @@ use std::cmp::PartialEq;
 
 use crate::SkVec;
 
-macro_rules! impl_inside {
-	($self:ident, $other:ident) => {{
-		if $self.len() != $other.len() {
-			return false;
-		}
+macro_rules! impl_eq {
+	($this:ty, $other:ty $(, $($extra:tt)*)?) => {
+		impl<T: PartialEq $(, $($extra)*)?> PartialEq<$other> for $this {
+			fn eq(&self, other: &$other) -> bool {
+				if self.len() != other.len() {
+					return false;
+				}
 
-		for (a, b) in $self.iter().zip($other.iter()) {
-			if a != b {
-				return false;
+				for (a, b) in self.iter().zip(other.iter()) {
+					if a != b {
+						return false;
+					}
+				}
+
+				true
 			}
 		}
-
-		true
-	}};
+	};
 }
 
-impl<T: PartialEq> PartialEq for SkVec<T> {
-	fn eq(&self, other: &Self) -> bool {
-		impl_inside!(self, other)
-	}
-}
+impl_eq!(SkVec<T>, SkVec<T>);
 
-impl<T: PartialEq> PartialEq<[T]> for SkVec<T> {
-	fn eq(&self, other: &[T]) -> bool {
-		impl_inside!(self, other)
-	}
-}
+impl_eq!(SkVec<T>, Vec<T>);
+impl_eq!(SkVec<T>, [T]);
+impl_eq!(SkVec<T>, &[T]);
+impl_eq!(SkVec<T>, [T; N], const N: usize);
 
-impl<T: PartialEq, const N: usize> PartialEq<[T; N]> for SkVec<T> {
-	fn eq(&self, other: &[T; N]) -> bool {
-		impl_inside!(self, other)
-	}
-}
+impl_eq!(Vec<T>, SkVec<T>);
+impl_eq!([T], SkVec<T>);
+impl_eq!(&[T], SkVec<T>);
+impl_eq!([T; N], SkVec<T>, const N: usize);
 
-impl<T: PartialEq> PartialEq<Vec<T>> for SkVec<T> {
-	fn eq(&self, other: &Vec<T>) -> bool {
-		impl_inside!(self, other)
-	}
-}
-
-impl<T: PartialEq> PartialEq<SkVec<T>> for [T] {
-	fn eq(&self, other: &SkVec<T>) -> bool {
-		impl_inside!(self, other)
-	}
-}
-
-impl<T: PartialEq, const N: usize> PartialEq<SkVec<T>> for [T; N] {
-	fn eq(&self, other: &SkVec<T>) -> bool {
-		impl_inside!(self, other)
-	}
-}
-
-impl<T: PartialEq> PartialEq<SkVec<T>> for Vec<T> {
-	fn eq(&self, other: &SkVec<T>) -> bool {
-		impl_inside!(self, other)
-	}
-}
+impl<T: Eq> Eq for SkVec<T> {}
