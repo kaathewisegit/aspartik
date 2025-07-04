458c05154b9f7aa76ee222684feb7db064976f24
Andrej Kolčin <self@kaathewise.net>
2025-07-04 22:34:29 +0300

    [stats] distributions: add Poisson Python PMF and CDF tests
    I think there might be a point in setting `num` in `random_` via a flag.
    It would enable me to run uber-tests with thousands of cases without
    making the default test suite too slow.  The only thing I'm not sure
    about is how to handle product parametrization.  Overwriting `num` would
    require me to re-import the global setting everywhere, which is verbose.
    So a `ratio` argument is probably better.

--- python/tests/stats/distributions/poisson.py
+++ python/tests/stats/distributions/poisson.py
@@ -1,15 +1,19 @@
 import pytest
-
-from math import isclose, nan
+from math import nan
+from mpmath import mp
+from tests.utils import random_integer
+from aspartik.math import is_close
 
 from aspartik.stats.distributions import Poisson, PoissonError
+
+mp.dps = 1000
 
 
 def test_basic():
     d = Poisson(1)
     assert d.lambda_ == 1
     assert repr(d) == "Poisson(1)"
-    assert isclose(0.367879441171442, d.pmf(1), rel_tol=1e-15)
+    assert is_close(0.367879441171442, d.pmf(1), relative=1e-15)
     assert d.lower == 0
     assert d.upper == 2**64 - 1  # u64::MAX
 
@@ -26,3 +30,29 @@ def test_errors():
     with pytest.raises(ValueError) as error:
         Poisson(nan)
     assert error.value.args[0] == PoissonError.LambdaInvalid
+
+
+@pytest.mark.parametrize("lambda_", [1, 4, 10])
+@pytest.mark.parametrize("x", random_integer(0, 20, num=250))
+def test_pmf(lambda_, x):
+    d = Poisson(lambda_)
+
+    mp_lambda = mp.mpf(lambda_)
+    mp_x = mp.mpf(x)
+    expected = (mp_lambda**mp_x * mp.e ** (-mp_lambda)) / mp.factorial(mp_x)
+    expected = float(expected)
+
+    assert is_close(d.pmf(x), expected, relative=1e-14)
+
+
+@pytest.mark.parametrize("lambda_", [1, 4, 10])
+@pytest.mark.parametrize("x", random_integer(0, 20, num=250))
+def test_cdf(lambda_, x):
+    d = Poisson(lambda_)
+
+    mp_lambda = mp.mpf(lambda_)
+    mp_x = mp.mpf(x)
+    expected = mp.gammainc(mp_x + 1, mp_lambda, regularized=True)
+    expected = float(expected)
+
+    assert is_close(d.cdf(x), expected, relative=1e-13)
--- python/tests/utils.py
+++ python/tests/utils.py
@@ -1,6 +1,13 @@
+from typing import List
+
 from aspartik.rng import RNG
 
 
-def random_float(lower, upper, num=100):
+def random_float(lower: float, upper: float, num: int = 100) -> List[float]:
     rng = RNG(4)
     return [rng.random_float() for _ in range(num)]
+
+
+def random_integer(lower: int, upper: int, num: int = 100) -> List[int]:
+    rng = RNG(4)
+    return [rng.random_int(lower, upper) for _ in range(num)]
