49d409200ecb3fcfd51dee03600903800ee20da6
Andrej Kolčin <self@kaathewise.net>
2025-07-12 11:21:53 +0300

    toolkit: replace with a Python package
    I'll be expanding the toolkit in the future, because there are a bunch
    of things I want to do:
    
    - Add a testing helper so I can run tests just by their name
    
    - Building all the crates standalone to check that the features are
      correct, which could really use a for loop
    
    - Configure test length for my Python simulation test, which will
      require tinkering with `pytest` options

---
+++ python/toolkit/__init__.py
@@ -0,0 +1,108 @@
+from argparse import ArgumentParser
+from shutil import rmtree
+import subprocess
+import sys
+from pathlib import Path
+
+
+def parser():
+    parser = ArgumentParser(
+        prog="toolkit",
+        description="Command runner for working on Aspartik",
+    )
+    subparsers = parser.add_subparsers(dest="subcommand")
+
+    subparsers.add_parser("lint", help="validate with linters and formatters")
+    subparsers.add_parser("test", help="run tests")
+    subparsers.add_parser("run", help="run a minimal `b3` simulation")
+    check = subparsers.add_parser("check", help="run all checks")  # noqa: F841
+    subparsers.add_parser("clean", help="remove temporary files and `b3` output")
+
+    return parser
+
+
+def execute(*args):
+    result = subprocess.run(args)
+
+    if result.returncode != 0:
+        sys.exit(
+            f"Command `{' '.join(args)}` failed with exit code {result.returncode}"
+        )
+
+
+def lint():
+    execute("cargo", "fmt", "--check")
+    subprocess.run(
+        [
+            "cargo",
+            "clippy",
+            "--workspace",
+            "--tests",
+            "--features",
+            "approx,proptest",
+            "--",
+            "-D",
+            "warnings",
+        ]
+    )
+
+    execute("ruff", "format", "--check")
+    execute("ruff", "check")
+
+    execute("pyright")
+
+
+def test():
+    execute("cargo", "test", "--workspace", "--features", "approx,proptest")
+
+    execute("pytest")
+
+
+def run():
+    execute("maturin", "develop", "--release")
+    execute("python", "benches/primate.py")
+
+
+ARTIFACTS = [
+    "flamegraph.svg",
+    "perf.data",
+    "perf.data.old",
+    "b3.trees",
+    "b3.log",
+    ".pytest_cache",
+]
+
+
+def check():
+    lint()
+    test()
+    run()
+
+
+def clean():
+    execute("ruff", "clean")
+
+    for path in Path(".").glob("python/**/__pycache__/"):
+        rmtree(path)
+
+    for path in map(Path, ARTIFACTS):
+        if path.is_file():
+            path.unlink()
+        elif path.is_dir():
+            rmtree(path)
+
+
+def main():
+    args = parser().parse_args()
+
+    match args.subcommand:
+        case "lint":
+            lint()
+        case "test":
+            test()
+        case "run":
+            run()
+        case "check":
+            check()
+        case "clean":
+            clean()
