b28e0d0930ebff56651fcde0f31cc9541bc5220c
Andrej Kolčin <self@kaathewise.net>
2025-08-31 14:47:22 +0300

    [bitmap] Add methods and more derives

--- crates/bitmap/src/lib.rs
+++ crates/bitmap/src/lib.rs
@@ -1,4 +1,6 @@
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Bitmap {
 	inner: Box<[u8]>,
 }
@@ -11,13 +13,21 @@ impl Bitmap {
 		}
 	}
 
-	pub fn set(&mut self, index: usize, value: bool) {
+	pub fn set(&mut self, index: usize, on: bool) {
 		let byte_index = index / 8;
-		let bit_value = u8::from(value) << (index % 8);
+		let bit_value = u8::from(on) << (index % 8);
 		self.inner[byte_index] |= bit_value;
 	}
 
-	pub fn contains(&self, index: usize) -> bool {
+	pub fn set_on(&mut self, index: usize) {
+		self.set(index, true)
+	}
+
+	pub fn set_off(&mut self, index: usize) {
+		self.set(index, false)
+	}
+
+	pub fn at(&self, index: usize) -> bool {
 		let byte_index = index / 8;
 		let bit_value = 1 << (index % 8);
 		(self.inner[byte_index] & bit_value) > 0
@@ -26,6 +36,12 @@ impl Bitmap {
 	pub fn clear(&mut self) {
 		for byte in &mut self.inner {
 			*byte = 0;
+		}
+	}
+
+	pub fn set_all_on(&mut self) {
+		for byte in &mut self.inner {
+			*byte = 0b1111_1111;
 		}
 	}
 }
@@ -41,11 +57,11 @@ mod test {
 		b.set(2, true);
 		b.set(9, true);
 
-		assert!(b.contains(1));
-		assert!(b.contains(2));
-		assert!(b.contains(9));
-		assert!(!b.contains(0));
-		assert!(!b.contains(3));
-		assert!(!b.contains(4));
+		assert!(b.at(1));
+		assert!(b.at(2));
+		assert!(b.at(9));
+		assert!(!b.at(0));
+		assert!(!b.at(3));
+		assert!(!b.at(4));
 	}
 }
