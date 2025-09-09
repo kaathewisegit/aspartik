0a85a3ec2d8dd3254d60215631c8601269e21ea7
Andrej Kolčin <self@kaathewise.net>
2025-09-09 13:49:53 +0300

    [math] function/factorial: tweak docs

--- crates/math/src/function/factorial.rs
+++ crates/math/src/function/factorial.rs
@@ -6,21 +6,20 @@ use pyo3::prelude::*;
 
 use crate::function::gamma;
 
-/// The maximum factorial representable
-/// by a 64-bit floating point without
-/// overflowing
+/// Largest factorial representable by a 64-bit floating point without an
+/// overflow.
 pub const MAX_FACTORIAL: usize = 170;
 
-/// Factorial function: `x!`
+/// `x!`
 ///
-/// Gives exact values for `x <= 170`.  For larger inputs returns infnity.
+/// Uses a lookup map for `x <= 170` and returns infnity for larger `x`.
 #[cfg_attr(feature = "python", pyfunction)]
 pub fn factorial(x: u64) -> f64 {
 	let x = x as usize;
 	FCACHE.get(x).map_or(f64::INFINITY, |&fac| fac)
 }
 
-/// Logarithmic factorial function: `ln(x!)`
+/// `ln(x!)`
 ///
 /// Returns `0.0` if `x <= 1`.
 #[cfg_attr(feature = "python", pyfunction)]
@@ -32,9 +31,9 @@ pub fn ln_factorial(x: u64) -> f64 {
 	)
 }
 
-/// Binomial coefficient: `n choose k`
+/// Binomial coefficient
 ///
-/// Returns `0.0` if `k > n`
+/// `n` choose `k` or `C(n, k)`.  Returns `0.0` if `k > n`.
 #[cfg_attr(feature = "python", pyfunction)]
 pub fn binomial(n: u64, k: u64) -> f64 {
 	if k > n {
@@ -49,7 +48,7 @@ pub fn binomial(n: u64, k: u64) -> f64 {
 
 /// Natural logarithm of the binomial coefficient
 ///
-/// Returns `f64::NEG_INFINITY` if `k > n`
+/// Returns negative infinity if `k > n`.
 #[cfg_attr(feature = "python", pyfunction)]
 pub fn ln_binomial(n: u64, k: u64) -> f64 {
 	if k > n {
@@ -59,9 +58,11 @@ pub fn ln_binomial(n: u64, k: u64) -> f64 {
 	}
 }
 
-/// Computes the multinomial coefficient: `n choose k_1, k_2, k_3, ...`
+/// [Multinomial coefficient][w]
 ///
 /// Returns `None` if the elements in `ks` do not sum to `n`.
+///
+/// [w]: https://en.wikipedia.org/wiki/Multinomial_theorem#Multinomial_coefficients
 pub fn multinomial(n: u64, ks: &[u64]) -> Option<f64> {
 	let (sum, ret) = ks.iter().fold((0, ln_factorial(n)), |acc, &x| {
 		(acc.0 + x, acc.1 - ln_factorial(x))
@@ -78,7 +79,8 @@ pub fn multinomial(n: u64, ks: &[u64]) -> Option<f64> {
 const FCACHE: [f64; MAX_FACTORIAL + 1] = {
 	let mut fcache = [1.0; MAX_FACTORIAL + 1];
 
-	// `const` only allow while loops
+	// `const` only allow while loops (because `next` on `Iterator` isn't
+	// `const`)
 	let mut i = 1;
 	while i < MAX_FACTORIAL + 1 {
 		fcache[i] = fcache[i - 1] * i as f64;
