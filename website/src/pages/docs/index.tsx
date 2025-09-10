63863727a0fef3d961a07e970cbe0b54106cf1f9
Andrej Kolčin <self@kaathewise.net>
2025-09-10 18:04:03 +0300

    [website] Add subpage descriptions to the docs index

--- website/src/pages/docs/index.tsx
+++ website/src/pages/docs/index.tsx
@@ -4,21 +4,44 @@ export default function () {
 	return (
 		<Html class="flex min-h-screen items-center justify-center">
 			<div class="flex w-full max-w-5xl flex-col gap-y-4 md:grid md:grid-cols-2 md:gap-x-8">
-				<Container title="Tutorials" href="./tutorials" />
-				<Container title="How-to guides" href="./howto" />
-				<Container title="Explanation" href="./explanation" />
-				<Container title="Reference" href="./reference" />
+				<Container
+					title="Tutorials"
+					href="./tutorials"
+					desc="Step by step lessons"
+				/>
+				<Container
+					title="How-to guides"
+					href="./howto"
+					desc="Solutions to concrete tasks"
+				/>
+				<Container
+					title="Explanation"
+					href="./explanation"
+					desc="Architecture and theory notes"
+				/>
+				<Container
+					title="Reference"
+					href="./reference"
+					desc="Description of the Python API"
+				/>
 			</div>
 		</Html>
 	)
 }
 
-export function Container(props: { title: string; href: string }): JSX.Element {
+export function Container(props: {
+	title: string
+	href: string
+	desc: string
+}): JSX.Element {
 	return (
-		<div class="flex h-48 items-center justify-center border lg:w-128">
+		<div class="flex flex-col h-48 items-center text-center justify-center border lg:w-128">
 			<h2 class="text-xl lg:text-3xl">
-				<a href={props.href}>{props.title}</a>
+				<a class="" href={props.href}>
+					{props.title}
+				</a>
 			</h2>
+			<p class="lg:text-xl mt-4">{props.desc}</p>
 		</div>
 	)
 }
