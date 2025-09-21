81a7c84f8d3c1d1f77b94eff4ac182f506d5b443
Andrej Kolčin <self@kaathewise.net>
2025-09-21 11:32:35 +0300

    [b3] substitutions: reduplicate frequencies checks and other tweaks

--- python/aspartik/b3/substitutions.py
+++ python/aspartik/b3/substitutions.py
@@ -1,18 +1,28 @@
 from dataclasses import dataclass, field
 from math import prod
-from typing import ClassVar, List, SupportsFloat, Tuple
+from typing import ClassVar, List, SupportsFloat
 
 from aspartik.math import is_close
 
+type Float4 = tuple[float, float, float, float]
+type Float6 = tuple[float, float, float, float, float, float]
+# TODO: either a `linalg` type or something else static
+type Matrix = List[List[float]]
 
-def normalize(matrix: List[List[float]], coef: float) -> List[List[float]]:
+
+def normalize(matrix: Matrix, coef: float) -> Matrix:
     return [[element / coef for element in row] for row in matrix]
+
+
+def check_frequencies(f: Float4) -> None:
+    if not is_close(sum(f), 1.0):
+        raise ValueError(f"The sum of frequencies must be 1, got {sum(f)}")
 
 
 @dataclass(slots=True)
 class JC:
     dimensions: ClassVar[int] = 4
-    matrix: ClassVar[List[List[float]]] = normalize(
+    matrix: ClassVar[Matrix] = normalize(
         [
             [-3, 1, 1, 1],
             [1, -3, 1, 1],
@@ -22,7 +32,7 @@ class JC:
         3,
     )
 
-    def get_matrix(self):
+    def get_matrix(self) -> Matrix:
         return self.matrix
 
 
@@ -35,7 +45,7 @@ class K80:
         # TODO: check that kappa is a single-dimensional real
         pass
 
-    def get_matrix(self):
+    def get_matrix(self) -> Matrix:
         k = float(self.kappa)
         s = [
             [-2 - k, 1, k, 1],
@@ -51,12 +61,13 @@ class K80:
 @dataclass(slots=True)
 class F81:
     dimensions: ClassVar[int] = 4
-    frequencies: Tuple[float, float, float, float]
-    # TODO: either a `linalg` type or something else static
-    _matrix: List[List[float]]
+    frequencies: Float4
+    _matrix: Matrix
 
     def __post_init__(self):
-        # XXX: perhaps the frequencies should be made dynamic
+        check_frequencies(self.frequencies)
+
+        # XXX: are dynamic frequencies used in real-world analysis?
         a, c, g, t = self.frequencies
         s = [
             [a - 1, c, g, t],
@@ -66,26 +77,24 @@ class F81:
         ]
         self._matrix = normalize(s, 1 - a**2 - c**2 - g**2 - t**2)
 
-    def get_matrix(self):
+    def get_matrix(self) -> Matrix:
         return self._matrix
 
 
 @dataclass(slots=True)
 class HKY:
     dimensions: ClassVar[int] = 4
-    frequencies: Tuple[float, float, float, float]
+    frequencies: Float4
     kappa: SupportsFloat
-    _cached_matrix: List[List[float]] = field(default_factory=list, init=False)
+    _cached_matrix: Matrix = field(default_factory=list, init=False)
     _cached_kappa: float = field(default=0.0, init=False)
 
     def __post_init__(self):
-        if not is_close(sum(self.frequencies), 1.0):
-            s = sum(self.frequencies)
-            raise ValueError(f"Sum of frequencies must be 1, got {s}")
+        check_frequencies(self.frequencies)
 
         self._update_matrix()
 
-    def _update_matrix(self):
+    def _update_matrix(self) -> None:
         k = float(self.kappa)
 
         a, c, g, t = self.frequencies
@@ -107,20 +116,17 @@ class HKY:
         self._cached_matrix = s
         self._cached_kappa = k
 
-    def get_matrix(self):
+    def get_matrix(self) -> Matrix:
         if float(self.kappa) != self._cached_kappa:
             self._update_matrix()
 
         return self._cached_matrix
 
 
-type Float6 = tuple[float, float, float, float, float, float]
-
-
 @dataclass(slots=True)
 class GTR:
     dimensions: ClassVar[int] = 4
-    frequencies: Tuple[float, float, float, float]
+    frequencies: Float4
 
     # Same as BEAST2.  Packing the fields into a vector would complicate
     # manipulating each rate individually.
@@ -135,10 +141,7 @@ class GTR:
     _cached_rates: Float6 = field(default_factory=tuple, init=False)
 
     def __post_init__(self):
-        # TODO: deduplicate
-        if not is_close(sum(self.frequencies), 1.0):
-            s = sum(self.frequencies)
-            raise ValueError(f"Sum of frequencies must be 1, got {s}")
+        check_frequencies(self.frequencies)
 
         self._update_matrix()
 
@@ -152,7 +155,7 @@ class GTR:
             float(self.rate_gt),
         )
 
-    def _update_matrix(self):
+    def _update_matrix(self) -> None:
         a, c, g, t = self.frequencies
         rate_ac, rate_ag, rate_at, rate_cg, rate_ct, rate_gt = self._get_rates()
 
@@ -181,7 +184,7 @@ class GTR:
         self._cached_matrix = s
         self._cached_rates = self._get_rates()
 
-    def get_matrix(self):
+    def get_matrix(self) -> Matrix:
         if self._get_rates() != self._cached_rates:
             self._update_matrix()
 
