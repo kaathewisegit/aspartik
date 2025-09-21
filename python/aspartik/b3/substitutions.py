74c3a132e9164c7dc68ec75623f515fdafd06530
Andrej Kolčin <self@kaathewise.net>
2025-09-21 11:25:12 +0300

    [b3] substitutions: add GTR

--- python/aspartik/b3/substitutions.py
+++ python/aspartik/b3/substitutions.py
@@ -114,4 +114,75 @@ class HKY:
         return self._cached_matrix
 
 
-# TODO: GTR
+type Float6 = tuple[float, float, float, float, float, float]
+
+
+@dataclass(slots=True)
+class GTR:
+    dimensions: ClassVar[int] = 4
+    frequencies: Tuple[float, float, float, float]
+
+    # Same as BEAST2.  Packing the fields into a vector would complicate
+    # manipulating each rate individually.
+    rate_ac: float = field(default=1)
+    rate_ag: float = field(default=1)
+    rate_at: float = field(default=1)
+    rate_cg: float = field(default=1)
+    rate_ct: float = field(default=1)
+    rate_gt: float = field(default=1)
+
+    _cached_matrix: List[List[float]] = field(default_factory=list, init=False)
+    _cached_rates: Float6 = field(default_factory=tuple, init=False)
+
+    def __post_init__(self):
+        # TODO: deduplicate
+        if not is_close(sum(self.frequencies), 1.0):
+            s = sum(self.frequencies)
+            raise ValueError(f"Sum of frequencies must be 1, got {s}")
+
+        self._update_matrix()
+
+    def _get_rates(self) -> Float6:
+        return (
+            float(self.rate_ac),
+            float(self.rate_ag),
+            float(self.rate_at),
+            float(self.rate_cg),
+            float(self.rate_ct),
+            float(self.rate_gt),
+        )
+
+    def _update_matrix(self):
+        a, c, g, t = self.frequencies
+        rate_ac, rate_ag, rate_at, rate_cg, rate_ct, rate_gt = self._get_rates()
+
+        s = [
+            [0, rate_ac * c, rate_ag * g, rate_at * t],
+            [rate_ac * a, 0, rate_cg * g, rate_ct * t],
+            [rate_ag * a, rate_cg * c, 0, rate_ct * t],
+            [rate_at * a, rate_ct * c, rate_gt * g, 0],
+        ]
+
+        for i in range(4):
+            s[i][i] = -sum(s[i])
+
+        mult = (
+            rate_ac * a * c
+            + rate_ag * a * g
+            + rate_at * a * t
+            + rate_cg * c * g
+            + rate_ct * c * t
+            + rate_gt * g * t
+        )
+
+        scale = 1.0 / (2.0 * mult)
+        s = normalize(s, scale)
+
+        self._cached_matrix = s
+        self._cached_rates = self._get_rates()
+
+    def get_matrix(self):
+        if self._get_rates() != self._cached_rates:
+            self._update_matrix()
+
+        return self._cached_matrix
