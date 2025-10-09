15aaf6f5803cb69165d7549f6038df6b750cfc01
Andrej Kolčin <self@kaathewise.net>
2025-10-09 11:03:39 +0300

    [examples] primate: rework
    I picked the simplest and smallest model I could find.  This surfaced
    several issues:
    
    - Kappa runs away to infinity if it's not constrained.  Something is
      amiss with either the tree shape prior or the likelihood calculations.
    
    - Operator moves are off.  Kappa and birth rate sit at 0% for me, but
      that can be explained away by the artificial bound.  The moves are
      more puzzling.  BEAST's full scale has 17% acceptance rate, whereas
      for me it's always 0.  Root slide is 11% vs 24%, which is probably
      compensated by node slide hanging at 35% vs 20% for BEAST.  Wide
      exchange is 12% for me vs 0 for BEAST, which is also weird.
    
    - The shape of the tree is correct, but the length is off.  This goes
      back to the first kappa issue.  I'll have to drill into transitions.

--- python/examples/primate.py
+++ python/examples/primate.py
@@ -1,103 +1,86 @@
+"""
+<https://beast.community/first_tutorial>
+"""
+
 from aspartik.b3 import MCMC, Likelihood, Real, Tree
 from aspartik.b3.clocks import StrictClock
 from aspartik.b3.loggers import PrintLogger, TreeLogger, ValueLogger
 from aspartik.b3.operators import (
-    DeltaExchange,
-    EpochScale,
     NarrowExchange,
     NodeSlide,
     ParamScale,
-    SubtreePruneRegraft,
+    RootSlide,
     TreeScale,
     WideExchange,
     WilsonBalding,
 )
-from aspartik.b3.priors import ConstantPopulation, Distribution, Monophyly, Yule
+from aspartik.b3.priors import Bound, Distribution, Yule
 from aspartik.b3.substitutions import HKY
 from aspartik.b3.utils import print_operator_stats
 from aspartik.io.msa import read_msa_from_fasta
 from aspartik.rng import RNG
-from aspartik.stats.distributions import Exp, Gamma, LogNormal, Uniform
+from aspartik.stats.distributions import Gamma, LogNormal, Normal, Uniform
 
-msa = read_msa_from_fasta("crates/b3/data/primate-mdna-full.fasta").deduplicate()
+msa = read_msa_from_fasta("crates/b3/data/primate.fasta").deduplicate()
 
 rng = RNG(4)
 tree = Tree(msa.sequence_names(), rng)
 
-mutation_rate = Real(1.0)
-gamma_shape = Real(1.0)
 kappa = Real(2.0)
-
-population = Real(1.0)
-birth_rate_y = Real(1.0)
-
-params = [
-    mutation_rate,
-    gamma_shape,
-    kappa,
-    birth_rate_y,
-]
-
-
-homo, pan = tree.leaf_by_name("Homo_sapiens"), tree.leaf_by_name("Pan_troglodytes")
-assert homo != None
-assert pan != None
+yule_birth_rate = Real(1.0)
 
 priors = [
-    Yule(tree, birth_rate_y),
-    Distribution(birth_rate_y, Gamma(0.001, 1 / 1000.0)),
-    Distribution(gamma_shape, Exp(1.0)),
     Distribution(kappa, LogNormal(1.0, 1.25)),
-    ConstantPopulation(tree, population),
-    Monophyly(tree, [homo, pan]),
+    Distribution(yule_birth_rate, LogNormal(1.0, 1.25)),
+    Yule(tree, yule_birth_rate),
+    Bound(kappa, 0, 50),  # TODO: runaway parameter
 ]
 
-# TODO
 operators = [
-    ParamScale(gamma_shape, 0.5, Uniform(0, 1), rng, weight=1.0),
-    ParamScale(kappa, 0.1, Uniform(0, 1), rng, weight=0.1),
-    EpochScale(tree, 0.9, Uniform(0, 1), rng, weight=4.0),
-    NarrowExchange(tree, rng, weight=15.0),
-    WideExchange(tree, rng, weight=3.0),
-    WilsonBalding(tree, rng, weight=3.0),
-    NodeSlide(tree, Uniform(0, 1), rng, weight=15.0),
-    TreeScale(tree, 0.9, Uniform(0, 1), rng, weight=2.0),
-    SubtreePruneRegraft(tree, rng, weight=3),
-    ParamScale(birth_rate_y, 0.1, Uniform(0, 1), rng, weight=3),
+    ParamScale(kappa, 0.75, Uniform(0, 1), rng, weight=1),
+    TreeScale(tree, 0.75, Uniform(0, 1), rng, weight=3),
+    NodeSlide(tree, Normal(0, 1), rng, weight=30),
+    NarrowExchange(tree, rng, weight=30),
+    WideExchange(tree, rng, weight=3),
+    WilsonBalding(tree, rng, weight=3),
+    RootSlide(tree, 0.75, Uniform(0, 1), rng, weight=3),
+    # Uniform scale?
+    ParamScale(yule_birth_rate, 0.75, Uniform(0, 1), rng, weight=3),
 ]
 
 # TODO: frequencies from alignment
 sub_model = HKY((0.25, 0.25, 0.25, 0.25), kappa)
-clock_model = StrictClock(Real(1.0))
 likelihood = Likelihood(
     msa=msa,
     substitution=sub_model,
-    clock=clock_model,
+    clock=StrictClock(1.0),
     tree=tree,
-    calculator="thread",
+    calculator="cpu",
 )
 
 loggers = [
-    TreeLogger(tree=tree, path="target/b3.trees", every=1_000),
-    PrintLogger(every=1_000),
+    TreeLogger(tree=tree, path="target/primate.trees", every=1_000),
+    PrintLogger(every=10_000),
     ValueLogger(
         {
-            "mutation_rate": mutation_rate,
-            "gamma_shape": gamma_shape,
+            "step": lambda: mcmc.current_step,
+            "joint": lambda: mcmc.prior + mcmc.likelihood,
+            "prior": lambda: mcmc.prior,
+            "likelihood": lambda: mcmc.likelihood,
+            "tree:root_height": lambda: tree.height_of(tree.root),
+            "prior:kappa": priors[0],
             "kappa": kappa,
-            "birth_rate_y": birth_rate_y,
-            "population": population,
-            "coalescent": priors[-1],
+            "yule_birth_rate": yule_birth_rate,
         },
-        path="target/b3.log",
+        path="target/primate.log",
         every=1_000,
     ),
 ]
 
 mcmc = MCMC(
     burnin=0,
-    length=10_000,
-    state=params + [tree],
+    length=1_000_000,
+    state=[kappa, yule_birth_rate, tree],
     priors=priors,
     operators=operators,
     likelihoods=[likelihood],
@@ -106,5 +89,4 @@ mcmc = MCMC(
 )
 
 mcmc.run()
-
 print_operator_stats(mcmc)
