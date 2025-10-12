2179e074c950f21bc230d615b1fa38c2c49e6b37
Andrej Kolčin <self@kaathewise.net>
2025-10-12 18:02:36 +0300

    [b3] operators: fix `RootSlide` implementation

--- python/aspartik/b3/operators/_root.py
+++ python/aspartik/b3/operators/_root.py
@@ -5,7 +5,7 @@ from ...rng import RNG
 from ...stats.distributions import Distribution
 from .. import Operator, Proposal, Tree
 from ..tree import Internal, Node
-from ._util import scale_on_range
+from ._util import sample_range
 
 
 @dataclass(slots=True)
@@ -32,7 +32,10 @@ class RootSlide(Operator):
         root = tree.root
 
         low, high = self.factor, 1 / self.factor
-        new_height, ratio = scale_on_range(low, high, self.distribution, rng)
+        scale = sample_range(low, high, self.distribution, rng)
+
+        old_height = tree.height_of(root)
+        new_height = old_height * scale
 
         left, right = tree.children_of(root)
         if new_height < max(tree.height_of(left), tree.height_of(right)):
@@ -40,4 +43,4 @@ class RootSlide(Operator):
 
         tree.set_height(root, new_height)
 
-        return Proposal.Hastings(ratio)
+        return Proposal.Hastings(-log(scale))
