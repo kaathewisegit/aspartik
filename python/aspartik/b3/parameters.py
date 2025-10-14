2fea0f26756edd9ca69e5be32ed62b5671ce518e
Andrej Kolčin <self@kaathewise.net>
2025-10-14 22:36:01 +0300

    [b3] parameters: add `Internals` parameter wrapper

--- python/aspartik/b3/parameters.py
+++ python/aspartik/b3/parameters.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Protocol, runtime_checkable
 
 from .._aspartik_rust_impl._b3_rust_impl import (
@@ -5,10 +6,21 @@ from .._aspartik_rust_impl._b3_rust_impl import (
     Integer as Integer,
     Real as Real,
 )
+from . import Tree
 
 Parameter = Real | Integer | Boolean
 
 
 @runtime_checkable
 class Scalable(Protocol):
-    def scale(factor: float) -> int: ...
+    def scale(self, factor: float) -> int: ...
+
+
+@dataclass
+class Internals(Scalable):
+    tree: Tree
+
+    def scale(self, factor: float) -> int:
+        self.tree.scale(factor)
+
+        return self.tree.num_internals
