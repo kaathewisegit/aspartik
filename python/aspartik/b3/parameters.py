49e323bfe37917c7d1f9230a600c297995820d19
Andrej Kolčin <self@kaathewise.net>
2025-10-17 12:16:16 +0300

    [b3] parameters: add `Root` tree wrapper
    I used it in a poor man's prior on tree height.  But this might make
    `RootSlide` obsolete.

--- python/aspartik/b3/parameters.py
+++ python/aspartik/b3/parameters.py
@@ -29,3 +29,29 @@ class Internals(Scalable):
         self.tree.scale(factor)
 
         return self.tree.num_internals
+
+
+@dataclass
+class Root(Scalable):
+    tree: Tree
+
+    def scale(self, factor: float) -> int:
+        tree = self.tree
+        root = tree.root
+
+        old_height = tree.height_of(root)
+        new_height = old_height * factor
+        tree.set_height(root, new_height)
+
+        if factor < 1:
+            left, right = tree.children_of(root)
+            left_height, right_height = tree.height_of(left), tree.height_of(right)
+            if left_height > new_height or right_height > new_height:
+                raise ValueError(
+                    f"Scaling the root by {factor} will put it under one of its children"
+                )
+
+        return 1
+
+    def __float__(self) -> float:
+        return self.tree.height_of(self.tree.root)
