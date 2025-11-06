63cd4859b5728ad467e70b79239d5e75eebdec53
Andrej Kolčin <self@kaathewise.net>
2025-11-06 17:25:23 +0300

    [examples] respiratory: tweak to be closer to the BEAST example
    I suspect that the weights for the tree operators are too big.  Other
    parameters don't mix very well.
    
    Also, this model is still lacking a few important components.  The main
    ones are estimated frequencies and site heterogeneity.

--- python/examples/respiratory.py
+++ python/examples/respiratory.py
@@ -24,9 +24,9 @@ from aspartik.b3.operators import (
     UpDown,
 )
 from aspartik.b3.parameters import Internals, Real
-from aspartik.b3.priors import Distribution, ExponentialGrowth
-from aspartik.b3.substitutions import K80
-from aspartik.b3.utils import print_operator_stats
+from aspartik.b3.priors import Bound, Distribution, ExponentialGrowth
+from aspartik.b3.substitutions import HKY
+from aspartik.b3.utils import print_operator_stats, print_operator_timings
 from aspartik.data import newick
 from aspartik.io.msa import read_msa_from_fasta
 from aspartik.rng import RNG
@@ -37,9 +37,23 @@ msa = read_msa_from_fasta("crates/b3/data/b.1.1.7.fasta")
 rng = RNG(4)
 tree = Tree(msa.sequence_names(), rng)
 
-starting = newick.Tree(Path("crates/b3/data/respiratory-starting.tree").read_text())
-tree = Tree.from_newick(starting)
-tree.validate()
+times = []
+for name in msa.sequence_names():
+    node = tree.leaf_by_name(name)
+    assert node is not None
+
+    time_s = name.split("|")[-1]
+    time = datetime.strptime(time_s, "%Y-%m-%d")
+    times.append(time)
+
+max_time = max(times)
+
+for leaf, time in zip(tree.leaves(), times):
+    height = (max_time - time).days / 365
+    tree.set_height(leaf, height)
+
+tree.set_random_heights(0.001, rng)
+tree.accept()
 
 
 kappa = Real(2.0)
@@ -49,6 +63,9 @@ clock_rate = Real(0.001)
 params = [kappa, population_size, growth_rate, clock_rate]
 
 priors = [
+    Bound(kappa),
+    Bound(population_size),
+    Bound(clock_rate),
     Distribution(kappa, LogNormal(1.0, 1.25)),
     Distribution(clock_rate, Laplace(0, 0.5)),
     Distribution(population_size, Gamma(0.001, 1 / 1000.0)),
@@ -60,20 +77,16 @@ operators = [
     ParamScale(kappa, 0.75, Uniform(0, 1), rng, weight=1),
     ParamScale(clock_rate, 0.75, Uniform(0, 1), rng, weight=3),
     UpDown(Internals(tree), clock_rate, 0.75, Uniform(0, 1), rng, weight=3),
-    SubtreeLeap(tree, Normal(0, 1), rng, weight=30),
-    SubtreePruneRegraft(tree, rng, weight=30),
+    SubtreeLeap(tree, Normal(0, 1), rng, weight=1000),
+    SubtreePruneRegraft(tree, rng, weight=100),
     ParamScale(population_size, 0.75, Uniform(0, 1), rng, weight=3),
     RandomWalk(growth_rate, window=1.0, rng=rng, weight=3),
-    #
-    BeastNarrowExchange(tree, rng, weight=30),
-    RootSlide(tree, 0.75, Uniform(0, 1), rng, weight=3),
-    NodeSlide(tree, Uniform(0, 1), rng, weight=30),
 ]
 
 
 likelihood = Likelihood(
     msa=msa,
-    substitution=K80(kappa),
+    substitution=HKY((0.25, 0.25, 0.25, 0.25), kappa),
     clock=StrictClock(clock_rate),
     tree=tree,
     calculator="cuda",
@@ -89,17 +102,17 @@ loggers = [
             "joint": lambda: mcmc.prior + mcmc.cached_likelihood,
             "prior": lambda: mcmc.prior,
             "likelihood": lambda: mcmc.cached_likelihood,
+            "kappa": kappa,
             "population_size": population_size,
             "growth_rate": growth_rate,
             "clock_rate": clock_rate,
-            "kappa": kappa,
-            "prior:kappa": priors[0],
-            "prior:clock_rate": priors[1],
-            "prior:population_size": priors[2],
-            "prior:growth_rate": priors[3],
-            "prior:coalescent": priors[4],
             "tree:height": lambda: tree.height_of(tree.root),
             "tree:length": lambda: tree.total_length(),
+            "prior:kappa": priors[3],
+            "prior:clock_rate": priors[4],
+            "prior:population_size": priors[5],
+            "prior:growth_rate": priors[6],
+            "prior:coalescent": priors[7],
         },
         path="target/b3.log",
         every=1_000,
@@ -108,7 +121,7 @@ loggers = [
 
 mcmc = MCMC(
     burnin=0,
-    length=10_000_000,
+    length=100_000_000,
     state=params + [tree],
     priors=priors,
     operators=operators,
@@ -124,3 +137,5 @@ except Exception as e:
     print(e)
 
 print_operator_stats(mcmc)
+print()
+print_operator_timings(mcmc)
