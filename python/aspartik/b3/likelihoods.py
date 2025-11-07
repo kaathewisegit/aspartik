1c4a9c2a93750d543bbe05912d308a51760d6b76
Andrej Kolčin <self@kaathewise.net>
2025-11-07 18:43:08 +0300

    [b3] likelihood: add `WeightedLikelihood`

--- python/aspartik/b3/likelihoods.py
+++ python/aspartik/b3/likelihoods.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass
+
 from .._aspartik_rust_impl._b3_rust_impl import (
     CPU4Likelihood as CPU4Likelihood,
     CUDALikelihood as CUDALikelihood,
     Thread4Likelihood as Thread4Likelihood,
 )
 from . import Stateful
+from .parameters import Weights
 
 
 class Likelihood(Stateful):
@@ -19,3 +22,34 @@ class Likelihood(Stateful):
 
     def propose(self) -> None: ...
     def likelihood(self) -> float: ...
+    def cached_likelihood(self) -> float: ...
+
+
+@dataclass(slots=True)
+class WeightedLikelihood(Likelihood):
+    likelihoods: list[Likelihood]
+    weights: Weights
+
+    def propose(self) -> None:
+        for likelihood in self.likelihoods:
+            likelihood.propose()
+
+    def likelihood(self) -> float:
+        out = 0
+        for likelihood, weight in zip(self.likelihoods, self.weights):
+            out += likelihood.likelihood() * weight
+        return out
+
+    def cached_likelihood(self) -> float:
+        out = 0
+        for likelihood, weight in zip(self.likelihoods, self.weights):
+            out += likelihood.cached_likelihood() * weight
+        return out
+
+    def accept(self) -> None:
+        for likelihood in self.likelihoods:
+            likelihood.accept()
+
+    def reject(self) -> None:
+        for likelihood in self.likelihoods:
+            likelihood.reject()
