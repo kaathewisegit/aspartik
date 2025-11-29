87f4db8a617d98df658f7542ee668083ed61b151
Andrej Kolčin <self@kaathewise.net>
2025-11-29 17:08:50 +0300

    [b3/tests] Test `ExponentialGrowth` coalescent with a real BEAST2 run

---
+++ python/tests/b3/priors/exponential_growth.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pytest
+
+import re
+
+from aspartik.b3 import Tree
+from aspartik.b3.priors import ExponentialGrowth
+from aspartik.data.newick import Tree as NewickTree
+
+
+def test_matches_beast():
+    log_path = "data/runs/respiratory/log"
+    logs = pd.read_csv(log_path, sep="\t")
+
+    trees_path = "data/runs/respiratory/trees"
+    trees = open(trees_path, "r")
+
+    for (i, row), tree in zip(logs.iterrows(), trees):
+        population_size = row["population_size"]
+        growth_rate = row["growth_rate"]
+
+        tree = NewickTree(tree)
+        tree = Tree.from_newick(tree)
+
+        coalescent = ExponentialGrowth(tree, population_size, growth_rate)
+        diff = abs(coalescent.probability() - row["prior:coalescent"])
+
+        assert diff < 1e-10, f"Tree {i} has a diverging coalescent with diff {diff}"
