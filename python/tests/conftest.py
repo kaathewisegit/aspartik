2f34720561f5ce6e38aa7ceb280df46cee5a0651
Andrej Kolčin <self@kaathewise.net>
2026-01-15 12:33:54 +0300

    [tests] Add a `manual` mark for long-running tests

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import Config, TestReport, fixture
 
 from aspartik.rng import RNG
@@ -15,6 +16,16 @@ def pytest_report_teststatus(report: TestReport, config: Config):
     if _rng.random_bool(0.01):
         letter = "."
     return report.outcome, letter, report.outcome.upper()
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("-m") == "manual":
+        return
+
+    skip_manual = pytest.mark.skip(reason="need -m manual option to run")
+    for item in items:
+        if "manual" in item.keywords:
+            item.add_marker(skip_manual)
 
 
 @fixture
