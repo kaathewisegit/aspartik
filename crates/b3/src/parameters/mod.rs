945fe41618444e9cd22492c8e4efa4c0cd6859a4
Andrej Kolčin <self@kaathewise.net>
2026-01-28 14:39:18 +0300

    [b3] parameters: add concrete `PyParameter` enum

--- crates/b3/src/parameters/mod.rs
+++ crates/b3/src/parameters/mod.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use pyo3::prelude::*;
 
 mod class_vector;
 mod real;
@@ -18,4 +19,26 @@ pub trait Parameter {
 	fn accept(&mut self);
 
 	fn reject(&mut self);
+}
+
+pub enum PyParameter {
+	Tree(Py<PyTree>),
+	Real(Py<PyReal>),
+	ClassVector(Py<PyClassVector>),
+}
+
+impl<'py> FromPyObject<'_, 'py> for PyParameter {
+	type Error = PyErr;
+
+	fn extract(obj: Borrowed<'_, 'py, PyAny>) -> PyResult<Self> {
+		if let Ok(tree) = obj.cast::<PyTree>() {
+			Ok(Self::Tree(tree.into()))
+		} else if let Ok(real) = obj.cast::<PyReal>() {
+			Ok(Self::Real(real.into()))
+		} else if let Ok(class_vector) = obj.cast::<PyClassVector>() {
+			Ok(Self::ClassVector(class_vector.into()))
+		} else {
+			todo!("descriptive error")
+		}
+	}
 }
