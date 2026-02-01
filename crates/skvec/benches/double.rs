aa7313dab4d84bb34a5bddd4b44a8f75f8f92c1d
Andrej Kolčin <self@kaathewise.net>
2026-02-01 14:28:29 +0300

    [skvec] benches: add more benches

--- crates/skvec/benches/double.rs
+++ crates/skvec/benches/double.rs
@@ -20,9 +20,7 @@ fn data(length: usize) -> SkVec<Row> {
 }
 
 #[inline(always)]
-fn edit(v: &mut SkVec<[f64; 4]>, rng: &mut SmallRng) {
-	let num = v.len() / 10;
-
+fn edit(v: &mut SkVec<[f64; 4]>, num: usize, rng: &mut SmallRng) {
 	for _ in 0..num {
 		let i = rng.random_range(0..v.len());
 		v.set(i, rng.random());
@@ -30,21 +28,45 @@ fn edit(v: &mut SkVec<[f64; 4]>, rng: &mut SmallRng) {
 }
 
 #[divan::bench]
-fn edit_accept(bencher: Bencher) {
+fn edit_rows_tenth_accept(bencher: Bencher) {
 	bencher.with_inputs(|| black_box(data(100_000)))
 		.bench_values(|mut v| {
 			let mut rng = SmallRng::seed_from_u64(4);
-			edit(&mut v, &mut rng);
+			let num = v.len() / 10;
+			edit(&mut v, num, &mut rng);
 			v.accept();
 		})
 }
 
 #[divan::bench]
-fn edit_reject(bencher: Bencher) {
+fn edit_rows_tenth_reject(bencher: Bencher) {
 	bencher.with_inputs(|| black_box(data(100_000)))
 		.bench_values(|mut v| {
 			let mut rng = SmallRng::seed_from_u64(4);
-			edit(&mut v, &mut rng);
+			let num = v.len() / 10;
+			edit(&mut v, num, &mut rng);
+			v.reject();
+		})
+}
+
+#[divan::bench]
+fn edit_rows_all_accept(bencher: Bencher) {
+	bencher.with_inputs(|| black_box(data(100_000)))
+		.bench_values(|mut v| {
+			let mut rng = SmallRng::seed_from_u64(4);
+			let num = v.len();
+			edit(&mut v, num, &mut rng);
+			v.accept();
+		})
+}
+
+#[divan::bench]
+fn edit_rows_all_reject(bencher: Bencher) {
+	bencher.with_inputs(|| black_box(data(100_000)))
+		.bench_values(|mut v| {
+			let mut rng = SmallRng::seed_from_u64(4);
+			let num = v.len();
+			edit(&mut v, num, &mut rng);
 			v.reject();
 		})
 }
