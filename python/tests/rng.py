a8924fa0a913de7c7ab428725feb47f4a6be9e8d
Andrej Kolčin <self@kaathewise.net>
2026-02-22 20:31:23 +0300

    [rng] tests: replace unused test file

--- python/tests/rng.py
+++ python/tests/rng.py
@@ -1,5 +1,5 @@
 import pytest
-from utils import random_integer
+from utils import random_float, random_integer
 
 from aspartik.rng import RNG
 
@@ -24,3 +24,23 @@ def rng():
 def test_float_in_bounds(rng, upper, lower):
     for _ in range(1000):
         assert lower <= rng.random_float(lower=lower, upper=upper) <= upper
+
+
+@pytest.mark.parametrize("seed", random_integer(0, 2**63 - 1))
+def test_seed(seed: int):
+    rng = RNG(seed)
+    rng.random_bool()
+
+
+@pytest.mark.parametrize("ratio", [0, 1, *random_float(0, 1)])
+def test_bool(rng, ratio: float):
+    rng.random_bool(ratio)
+
+
+@pytest.mark.skip("Panics instead of throwing an exception")
+@pytest.mark.parametrize(
+    "ratio", [*random_float(1, 10**10), *random_float(-(10**10), 0)]
+)
+def test_bool_error(rng, ratio: float):
+    with pytest.raises(Exception) as e:
+        rng.random_bool(ratio)
--- python/utils/__init__.py
+++ python/utils/__init__.py
@@ -7,7 +7,7 @@ from aspartik.rng import RNG
 
 def random_float(lower: float, upper: float, num: int = 100) -> list[float]:
     rng = RNG(4)
-    return [rng.random_float() for _ in range(num)]
+    return [rng.random_float(lower, upper) for _ in range(num)]
 
 
 def random_integer(lower: int, upper: int, num: int = 100) -> list[int]:
