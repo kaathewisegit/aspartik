e89b544afdc40602937d3055d5fb004cf773a940
Andrej Kolčin <self@kaathewise.net>
2026-03-28 13:23:40 +0300

    [b3/tests] tree: increase random MSA size

--- python/tests/b3/tree.py
+++ python/tests/b3/tree.py
@@ -60,8 +60,7 @@ def test_swap_parents(tree, rng):
     assert b_parent == new_a_parent
 
 
-# TODO: raise when scaling gets re-implemented
-@pytest.mark.parametrize("msa", random_msas(10, 100, num=20))
+@pytest.mark.parametrize("msa", random_msas(10, 1000, num=20))
 def test_dump_load_mcmc(msa: MSA, rng: RNG):
     mcmc = b3_config(
         msa,
