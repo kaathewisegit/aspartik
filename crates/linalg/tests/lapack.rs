f559f1460a6a21b1301d81d1d8c6ea49231bb723
Andrej Kolčin <self@kaathewise.net>
2026-04-05 12:50:17 +0300

    [linalg/tests] lapack: add testing of arbitrary matrices
    Only symmetric matrices for now, because some random ones still cause
    very incorrect results.  From what I've seen they don't look like the
    substitution matrices, but it seems that there are a few inputs which
    can trip up LAPACK.

--- crates/linalg/tests/lapack.rs
+++ crates/linalg/tests/lapack.rs
@@ -1,25 +1,48 @@
+use arbtest::arbtest;
+
 use linalg::{
+	arbitrary::symmetric,
 	lapack::{eigen, inverse},
 	matrix::{Matrix, SquareMatrix},
 };
 use math::assert_almost_eq;
 
-#[test]
-fn reconstruction() {
-	type M = [[f64; 2]; 2];
+type M<const N: usize> = [[f64; N]; N];
 
-	let m = [[1.0, 2.0], [3.0, 4.0]];
-
+fn reconstruct<const N: usize>(m: M<N>) {
 	let (values, vectors) = eigen(&m);
-	let inv_vectors = inverse(&vectors);
+	let inv_vectors: M<N> = inverse(&vectors);
 
-	let diag: M = SquareMatrix::from_diagonal(values);
+	let diag: M<N> = SquareMatrix::from_diagonal(values);
 
-	let reconstructed: M = vectors.mul::<_, M>(diag).mul(inv_vectors);
+	let reconstructed: M<N> = vectors.mul::<_, M<N>>(diag).mul(inv_vectors);
 
-	for i in 0..2 {
-		for j in 0..2 {
-			assert_almost_eq!(*reconstructed.at(i, j), *m.at(i, j));
+	for i in 0..N {
+		for j in 0..N {
+			assert_almost_eq!(
+				*reconstructed.at(i, j),
+				*m.at(i, j),
+				relative = 1e-9
+			);
 		}
 	}
+}
+
+#[test]
+fn reconstruction_basic() {
+	let m = [[1.0, 2.0], [3.0, 4.0]];
+	reconstruct(m);
+}
+
+#[test]
+fn reconstruction_symmetric() {
+	arbtest(|u| {
+		let m = symmetric::<4>(u)?;
+
+		reconstruct(m);
+
+		Ok(())
+	})
+	.size_min(128)
+	.budget_ms(500);
 }
