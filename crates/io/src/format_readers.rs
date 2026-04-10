5134642d888c2710f8562f8e506aec76176e83bd
Andrej Kolčin <self@kaathewise.net>
2026-04-10 09:46:39 +0300

    [io] Accept `PathLike` objects in `read_msa_from_fasta`

--- crates/io/src/format_readers.rs
+++ crates/io/src/format_readers.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
 
+#[cfg(feature = "python")]
+use std::path::PathBuf;
 use std::{fs::File, path::Path};
 
 use crate::reader::StrReader;
@@ -19,6 +21,6 @@ pub fn read_msa_from_fasta<P: AsRef<Path>>(
 
 #[cfg(feature = "python")]
 #[pyfunction(name = "read_msa_from_fasta")]
-pub fn py_read_msa_from_fasta(path: &str) -> Result<PyMsa> {
+pub fn py_read_msa_from_fasta(path: PathBuf) -> Result<PyMsa> {
 	read_msa_from_fasta(path).map(PyMsa)
 }
--- python/tests/data/msa.py
+++ python/tests/data/msa.py
@@ -3,12 +3,19 @@ from pathlib import Path
 from aspartik.io import read_msa_from_fasta
 
 
+def test_pathlike():
+    class Pathlike:
+        def __fspath__(self):
+            return "data/alignments/apes.fasta"
+
+    read_msa_from_fasta(Pathlike())
+
+
 def test_eq():
     alignments = Path("data/alignments/")
 
     for fasta_file in alignments.glob("*.fasta"):
-        # TODO: accept paths and pathlike objects
-        a = read_msa_from_fasta(str(fasta_file))
-        b = read_msa_from_fasta(str(fasta_file))
+        a = read_msa_from_fasta(fasta_file)
+        b = read_msa_from_fasta(fasta_file)
 
         assert a == b
