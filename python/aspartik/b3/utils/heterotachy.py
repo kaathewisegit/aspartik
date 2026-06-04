da4eabd378e51d6d09730a7fb36b4cf148efd01c
Andrej Kolčin <self@kaathewise.net>
2026-06-04 13:01:01 +0300

    [b3] utils/heterotachy: split into site and pattern probabilities

--- python/aspartik/b3/utils/heterotachy.py
+++ python/aspartik/b3/utils/heterotachy.py
@@ -1,17 +1,32 @@
 import polars as pl
 
 
-def siteprobs(categories: pl.Series) -> pl.DataFrame:
+def pattern_probabilities(categories: pl.Series) -> pl.DataFrame:
     num_rows = len(categories)
-    num_sites = categories.arr.len()[0]
+    num_patterns = categories.arr.len()[0]
     n = categories.arr.max().max() + 1  # type:ignore
     name = categories.name
 
     return (
         categories.to_frame()
         .select(pl.col(name).arr.explode())
-        .with_columns(site=pl.int_range(0, pl.len(), dtype=pl.UInt32) % num_sites)
-        .group_by("site")
+        .with_columns(pattern=pl.int_range(0, pl.len(), dtype=pl.UInt32) % num_patterns)
+        .group_by("pattern")
         .agg([((pl.col(name) == i).sum() / num_rows).alias(f"c{i}") for i in range(n)])
+        .sort("pattern")
+    )
+
+
+def site_probabilities(
+    pattern_probs: pl.DataFrame, sites_to_patterns: list[int]
+) -> pl.DataFrame:
+    mapping_df = pl.select(
+        site=pl.int_range(0, len(sites_to_patterns), dtype=pl.UInt32),
+        pattern=pl.Series(sites_to_patterns, dtype=pl.UInt32),
+    )
+
+    return (
+        mapping_df.join(pattern_probs, on="pattern", how="left")
+        .drop("pattern")
         .sort("site")
     )
