acd1df677637f6e98aa041239da054a61e32b30e
Andrej Kolčin <self@kaathewise.net>
2026-08-31 22:18:23 +0300

    [io] Delete crate

--- crates/aspartik/src/lib.rs
+++ crates/aspartik/src/lib.rs
@@ -2,17 +2,9 @@ use pyo3::prelude::*;
 
 #[pymodule(name = "_aspartik_rust_impl")]
 pub mod pymodule {
-	use super::*;
-
 	#[pymodule_export]
 	use {
 		b3::pymodule as b3, data::pymodule as data,
-		distributions::pymodule as distributions, io::pymodule as io,
-		rng::pymodule as rng,
+		distributions::pymodule as distributions, rng::pymodule as rng,
 	};
-
-	#[pymodule_init]
-	fn init(_: &Bound<'_, PyModule>) -> PyResult<()> {
-		Ok(())
-	}
 }
